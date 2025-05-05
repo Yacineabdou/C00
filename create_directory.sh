733c55e2e982b5fc0cef4f37b3b45d49e2b588c5
yacine <issay584@email.com>
2025-05-05 23:05:44 +0100

    cette fois

--- create_directory.sh
+++ create_directory.sh
@@ -1,2 +1,19 @@
-// ça parmet de nous crée un répertoire
-mkdir test_directory
+#!/bin/bash
+
+# la création du répertoire nommé test_directory
+repertoire="test_directory"
+
+# Vérifiction du répertoire si il existe déjà
+if [ -d "$repertoire" ]; then
+  echo "Le répertoire '$repertoire' existe déjà."
+else
+  # Crée le répertoire s'il n'existe pas
+  mkdir "$repertoire"
+  if [ $? -eq 0 ]; then
+    echo "Le répertoire '$repertoire' a été créé avec succès."
+  else
+    echo "Erreur lors de la création du répertoire '$repertoire'."
+  fi
+fi
+
+exit 0
