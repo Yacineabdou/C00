b5fe1648125ab2e0d57d96525e5cbf52f16af232
yacine <issay584@email.com>
2025-05-06 15:27:28 +0100

    j'essaye encore

--- current_datetime.sh
+++ current_datetime.sh
@@ -1,2 +1,2 @@
-# ça permet d'afficher la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS".
+# ça permet d'afficher la date et l'heure actuelle au format "YYYY-MM-DD HH:MM:SS"
 echo $(date '+%Y-%m-%d %H:%M:%S')
