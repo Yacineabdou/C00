5c379bcf247d43fa540c829d429ee6f2de3a0b04
yacine <issay584@email.com>
2025-05-06 12:24:30 +0100

    bien

--- create_directory.sh
+++ create_directory.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script pour créer un dossier, un fichier et y écrire quelque chose
 
-mkdir -p test_directory
+mkdir -p test_directory[ -d "test_directory" ] && echo "Le répertoire existe déjà." || mkdir "test_directory" && echo "Le répertoire a été créé."
