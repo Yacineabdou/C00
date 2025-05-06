71a3860d7f9fe9287ce9436fd3a66e954a33c8e6
yacine <issay584@email.com>
2025-05-06 12:14:58 +0100

    vrai

--- create_directory.sh
+++ create_directory.sh
@@ -2,8 +2,3 @@
 # Script pour créer un dossier, un fichier et y écrire quelque chose
 
 mkdir -p test_directory
-cd test_directory
-touch info.txt
-echo "Bienvenue dans le fichier créé par un script Bash !"
-
-echo "Fichier info.txt créé avec succès dans test_directory."
