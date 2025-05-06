da80ba15150cbe6d6d58a6d18b8f032b9f468145
yacine <issay584@email.com>
2025-05-06 13:23:49 +0100

    coin

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,10 @@
 # Ce script permet de vérifier si un fichier exist ou pas
 #!/bin/bash
-echo "Saisissez le nom du fichier :"
-read fichier
- if [ -f "$fichier" ]
- then
-     echo "Le fichier existe"
- else
-     echo "Mon fichier n'existe pas"
- fi
+# echo "Saisissez le nom du fichier :"
+# read fichier
+#  if [ -f "$fichier" ]
+#  then
+#      echo "Le fichier existe"
+#  else
+#      echo "Mon fichier n'existe pas"
+#  fi
