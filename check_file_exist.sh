c586e46fe769ad5fe8766ca1b150e58e8507f848
yacine <issay584@email.com>
2025-05-07 13:10:19 +0100

    yes

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 # Ce script permet de vérifier si un fichier exist ou pas
- echo "Saisissez le nom du fichier :"
- read fichier
-  if [ -f "$fichier" ];
-  then
-      echo "Le fichier '$fichier' existe."
-  else
-     echo "Le fichier '$fichier' n'existe pas."
- fi
+echo "Saisissez le nom du fichier :"
+read fichier
+
+if [ -f "$fichier" ]; then
+    echo "Le fichier '$fichier' existe."
+else
+    echo "Le fichier '$fichier' n'existe pas."
+fi
--- create_directory.sh
+++ create_directory.sh
@@ -1,2 +1,3 @@
 # Script pour créer un dossier, un fichier et y écrire quelque chose
+
 mkdir -p test_directory
